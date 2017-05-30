6dcbb8ca149d37905f33bc42fe662f433bd85808
vladimirjeune <randyjeune@gmail.com>
2017-05-30 00:02:06 -0400

    feat: Start using a constructor rather than the properties.
    You will have an evolution from what we had in the beginning
    to where we are going.

--- src/Card.kt
+++ src/Card.kt
@@ -1,9 +1,13 @@
 /**
  * Created by vladimirjeune on 5/29/17.
  */
-class Card {
+class Card (value: Int, suit: String, faceUp: Boolean) {
     val value: Int = 0  // Or, we could just do this.
     val suit: String = ""
     var faceUp: Boolean = false
+
+    init {
+        // Just an example that you can use init.  Not needed here.
+    }
 
 }
