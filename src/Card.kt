e760d27b9e184ad4c045fc2b6a37c31c38d209f7
vladimirjeune <randyjeune@gmail.com>
2017-05-29 21:38:05 -0400

    feat: Show that we have another way to write that a getter just returns 1 value
    So, val value: Int = 0, instead of get() = 0 on the next line.

--- src/Card.kt
+++ src/Card.kt
@@ -2,7 +2,6 @@
  * Created by vladimirjeune on 5/29/17.
  */
 class Card {
-    val value: Int
-    get() = 0  // Function only contained 1 expr, so can write like this.
+    val value: Int = 0  // Or, we could just do this.
 
 }
