ff11b7dc54a53f5d90e20ced99874ded361cb10d
vladimirjeune <randyjeune@gmail.com>
2017-05-30 00:11:36 -0400

    feat: Delete redundant JavaCard class.  Remove unnecessary brackets from Ctor
    The brackets weren't needed since there was nothing in the body.

--- src/Card.kt
+++ src/Card.kt
@@ -1,9 +1,4 @@
 /**
  * Created by vladimirjeune on 5/29/17.
  */
-class Card (val value: Int, val suit: String, var faceUp: Boolean) {
-
-    // In order to get this to one line add val|var in front of
-    // Ctor parameters.  Later we can even delete the brackets.
-
-}
+class Card (val value: Int, val suit: String, var faceUp: Boolean)
