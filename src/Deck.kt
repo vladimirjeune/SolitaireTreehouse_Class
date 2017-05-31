a78ffb6f21110ac9717eec7470a999e12a9477f2
vladimirjeune <randyjeune@gmail.com>
2017-05-30 23:52:03 -0400

    feat: Make things look more normal.

--- src/Deck.kt
+++ src/Deck.kt
@@ -3,14 +3,16 @@
  */
 
 // You can define either whole function here, or anon fn, or lambda in Array ctor
-val cards: Array<Card> = Array(52, fun (i : Int) : Card {
-    val value = i % 13
-    val suit = when(i / 13) {
-        0 -> "Clubs"
-        1 -> "Diamonds"
-        2 -> "Hearts"
-        else -> "Spades"
-    }
+val cards: Array<Card> = Array(52,
+        fun (i : Int) : Card {
+            val value = i % 13
+            val suit = when(i / 13) {
+                0 -> "Clubs"
+                1 -> "Diamonds"
+                2 -> "Hearts"
+                else -> "Spades"
+            }
 
-    return Card(value, suit)
-})
+            return Card(value, suit)
+        }
+)
