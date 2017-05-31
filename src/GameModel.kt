42b01a479e931bafc5f188ed3eddfe1605eb852f
vladimirjeune <randyjeune@gmail.com>
2017-05-31 19:08:04 -0400

    feat: Add newly created FoundationPile to Model

--- src/GameModel.kt
+++ src/GameModel.kt
@@ -4,5 +4,11 @@
 class GameModel {
     val deck = Deck()
     val wastePile : MutableList<Card> = mutableListOf()
+    val foundationPiles : Array<FoundationPile> = arrayOf(
+            FoundationPile("Clubs"),
+            FoundationPile("Diamonds"),
+            FoundationPile("Hearts"),
+            FoundationPile("Spades")
+    )
 
 }
