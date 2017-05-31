3f79b071821fd9755ee52d432ff914f04497a8cf
vladimirjeune <randyjeune@gmail.com>
2017-05-31 00:09:38 -0400

    feat: Since function was just one expr just used the equals

--- src/Deck.kt
+++ src/Deck.kt
@@ -5,14 +5,13 @@
 // You can define either whole function here, or anon fn, or lambda in Array ctor
 val cards: Array<Card> = Array(52,
         fun (i : Int) : Card {
-            val value = i % 13
-            val suit = when(i / 13) {
-                0 -> "Clubs"
-                1 -> "Diamonds"
-                2 -> "Hearts"
-                else -> "Spades"
-            }
-
-            return Card(value, suit)
+            return Card(i % 13, getSuit(i))
         }
 )
+
+fun getSuit(i: Int): String = when(i / 13) {  // Function was just one expr so just used '='
+        0 -> "Clubs"
+        1 -> "Diamonds"
+        2 -> "Hearts"
+        else -> "Spades"
+    }
