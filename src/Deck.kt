f53c40dfc19dc6b55e5ce151794062616cf1b13e
vladimirjeune <randyjeune@gmail.com>
2017-05-31 18:04:28 -0400

    feat: Add ability to draw a card and reset deck

--- src/Deck.kt
+++ src/Deck.kt
@@ -1,10 +1,27 @@
+import java.util.*
+
 /**
  * Created by vladimirjeune on 5/30/17.
  */
 
 // You can define either whole function here, or anon fn, or lambda in Array ctor
 val cards: Array<Card> = Array(52, { Card(it % 13, getSuit(it)) })
+var cardsInDeck: MutableList<Card> = cards.toMutableList()
 
+fun drawCards(): Card = cardsInDeck.removeAt(0)
+
+/**
+ * Obtains a full deck of cards and shuffles them
+ */
+fun reset() {
+    cardsInDeck = cards.toMutableList()
+    Collections.shuffle(cardsInDeck)
+}
+
+/**
+ * Takes in an Int and returns the appropriate suit for our deck of cards
+ * @property i Int between 0 and 52
+ */
 fun getSuit(i: Int): String = when(i / 13) {  // Function was just one expr so just used '='. Now Anon
         0 -> "Clubs"
         1 -> "Diamonds"
