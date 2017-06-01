51023dfc518ef1c288d2e6c4b6bdd45bc21ad701
vladimirjeune <randyjeune@gmail.com>
2017-06-01 18:39:15 -0400

    feat: Do testing on TableauPile's add()

--- test/TableauPileTest.kt
+++ test/TableauPileTest.kt
@@ -8,6 +8,16 @@ import org.junit.Assert.*
 class TableauPileTest {
     @Test
     fun addCards() {
+        // arrange: objects you need
+        val tableauPile = TableauPile(mutableListOf(Card(12, spades)))  // Only has King of Spades
+        val cards = mutableListOf(Card(11, hearts))
+
+
+        // act: on those objects
+        tableauPile.addCards(cards)
+
+        // assert: you got what you expected
+        assertEquals(2, tableauPile.cards.size)  // Since the order matches the rules we should have 2 cards.
     }
 
     @Test
