98e92434775e8ce7b18e1780a0eaf88238b251ec
vladimirjeune <randyjeune@gmail.com>
2017-06-01 00:57:00 -0400

    feat: Modify Card class to make telling color mismatches easier to id.

--- src/Card.kt
+++ src/Card.kt
@@ -13,6 +13,9 @@ val diamonds = "Diamonds"
 val hearts = "Hearts"
 val spades = "Spades"
 
+val redSuits = arrayOf(diamonds, hearts)
+val blackSuits = arrayOf(clubs, spades)
+
 class Card (val value: Int, val suit: String, var faceUp: Boolean = false) {
 
 }
