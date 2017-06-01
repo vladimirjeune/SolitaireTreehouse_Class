9f9be8fe8b108f6fe016fb698110cf64f8a756da
vladimirjeune <randyjeune@gmail.com>
2017-06-01 17:25:50 -0400

    feat: Finish up adding a card and deal with special case of empty pile
    If the pile is empty only a KING can take that position.  So it has to be
    the first card in the new cards that are being added.

--- src/TableauPile.kt
+++ src/TableauPile.kt
@@ -8,10 +8,17 @@ class TableauPile(var cards: MutableList<Card>) {
     }
 
     fun addCard(newCards: MutableList<Card>) :Boolean {
-        if ((newCards.first().value == cards.last().value - 1)
-        && (suitCheck(newCards.first(), cards.last())) ) {
-
+        if (cards.size > 0) {  // Tableau Pile is not empty
+            if ((newCards.first().value == cards.last().value - 1)
+                    && (suitCheck(newCards.first(), cards.last()))) {
+                cards.addAll(newCards)
+                return true
+            }
+        } else if (newCards.first().value == 12) {  // Tableau Pile is mt, and the 1st new card is KING
+            cards.addAll(newCards)
+            return true
         }
+        return false
     }
 
     /**
@@ -19,6 +26,10 @@ class TableauPile(var cards: MutableList<Card>) {
      *
      */
     private fun  suitCheck(c1: Card, c2: Card): Boolean {
-
+        if ((redSuits.contains(c1.suit) && blackSuits.contains(c2.suit))
+                || (blackSuits.contains(c1.suit) && redSuits.contains(c2.suit))) {
+            return true
+        }
+        return false
     }
 }
