9d9c06c9b1e8a9aaf58f4734def2eff694ee1518
vladimirjeune <randyjeune@gmail.com>
2017-06-03 00:02:20 -0400

    feat: Show simple Demo of Extension Functions
    After implemented, it should be able to run App.kt and return true.

--- src/App.kt
+++ src/App.kt
@@ -2,5 +2,6 @@
  * Created by vladimirjeune on 5/28/17.
  */
 fun main(args: Array<String>) {
-
+    val treehouse = "TreeHouse"
+    println(treehouse.isLongerThan5())
 }
--- src/GameModel.kt
+++ src/GameModel.kt
@@ -3,22 +3,25 @@
  */
 class GameModel {
     val deck = Deck()
-    val wastePile : MutableList<Card> = mutableListOf()
-    val foundationPiles : Array<FoundationPile> = arrayOf(
+    val wastePile: MutableList<Card> = mutableListOf()
+    val foundationPiles: Array<FoundationPile> = arrayOf(
             FoundationPile(clubs),
             FoundationPile(diamonds),
             FoundationPile(hearts),
             FoundationPile(spades)
     )
-    val tableauPiles = Array(7, {TableauPile()})
+    val tableauPiles = Array(7, { TableauPile() })
 
     fun resetGames() {
         wastePile.clear()
 
-    // Also acceptable: foundationPiles.forEach( { it.reset() } )  // same meaning; shows lamnda just a param
-    foundationPiles.forEach { it.reset() }
-
+        // Also acceptable: foundationPiles.forEach( { it.reset() } )  // same meaning; shows lamnda just a param
+        foundationPiles.forEach { it.reset() }
 
     }
-
 }
+    fun String.isLongerThan5(): Boolean {
+        return this.length > 5
+    }
+
+
