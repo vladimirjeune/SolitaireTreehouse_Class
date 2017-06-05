4b53da8f96dc7aa9bb3feea65b4a2a200e1c32a2
vladimirjeune <randyjeune@gmail.com>
2017-06-04 23:23:56 -0400

    feat: Use Ternary operator and String print, along with conditionals

--- src/Card.kt
+++ src/Card.kt
@@ -19,8 +19,18 @@ val blackSuits = arrayOf(clubs, spades)
 val cardsMap = mapOf(0 to "A", 1 to "2", 2 to "3" , 3 to "4", 4 to "5", 5 to "6", 6 to "7", 7 to "8", 8 to "9", 9 to "10", 10 to "J", 11 to "Q", 12 to "K")
 
 data class Card (val value: Int, val suit: String, var faceUp: Boolean = false) {
-    override fun toString(): String {
-        return "${cardsMap[value]}, $suit, $faceUp"
+    override fun toString(): String =
+        // There is NO ternary operator in Kotlin
+        if (faceUp) "${cardsMap[value]}, ${getSuitChar(suit)}" else "xxx"
+
+
+
+    private fun  getSuitChar(suit: String): String = when (suit) {
+        diamonds -> "\u2666"
+        diamonds -> "\u2663"
+        diamonds -> "\u2665"
+        diamonds -> "\u2660"
+        else -> "incorrect suit"
     }
 
 }
